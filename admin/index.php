1bc3cfcc5bbda59c666181c1da2c24abbbf93e65
Yuri santos <yuri-teixeira@outlook.com>
2023-04-25 22:42:33 -0300

    projeto20223

---
+++ admin/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <?php include "template/head.php" ?>
+  <body>
+    <?php include "template/navbar.php" ?>
+
+    <div class="container-fluid">
+        <div class="row">
+            <?php include "template/sidebar.php" ?>
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                
+            </main>
+        </div>
+    </div>
+
+    <?php include "template/footer.php" ?>
+  </body>
+</html>
--- pages/pizzas/cadastrar.php
+++ pages/pizzas/cadastrar.php
@@ -1,28 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>formulario</title>
 </head>
 <body>
     <form action="salvar.php" method="POST">
-        <div>
-            <label for="">Nome</label>
-            <input type="text" name="nome">
+        <div class="box_input">
+            <label class="text_label" for="">Nome</label>
+            <input class="input" type="text" name="nome">
         </div>
-        <div>
-            <label for="">Valor</label>
-            <input type="number" name="valor" step="0.01">
+        <div class="box_input">
+            <label class="text_label" for="">Valor</label>
+            <input class="input" type="number" name="valor" step="0.01">
         </div>
-        <div>
-            <label for="">Descrição</label>
+        <div class="box_input">
+            <label class="text_label" for="">Descrição</label>
             <textarea name="descricao"></textarea> 
         </div>
-        <div>
-            <label for="">Ingredientes</label>
-            <textarea name="ingredientes"></textarea>
+        <div class="box_input">
+            <label class="text_label" for="">Ingredientes</label>
+            <textarea class="input" name="ingredientes"></textarea>
         </div>
         <input type="submit" value="Cadastrar">
     </form>
